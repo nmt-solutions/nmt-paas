9e7888b92d2a3174b5f50315067a2422d3fd2a10
Tauqeer-Ahmed-99 <tauqueerrkhan@gmail.com>
2026-08-22 17:11:50 +0530

    🐞 fix: removed unwanted code

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,22 +1,11 @@
 import { redis } from "@repo/redis";
 import "./workers/deployment.worker.js";
-import { getDocker } from "./services/docker.js";
 
 const bootstrap = async () => {
   redis
     .ping()
     .then(async (res) => {
       console.log(res);
-
-      const docker = await getDocker();
-
-      const info = await docker.info();
-
-      console.log({
-        containers: info.Containers,
-        images: info.Images,
-        architecture: info.Architecture,
-      });
 
       console.log("Deployment worker started");
     })
